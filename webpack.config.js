70f33ee73b311d481254f11ee853ce24fc06a538
blessedjasonmwanza <mwanzabj@gmail.com>
2021-10-27 11:32:24 +0200

    Fix eslint and webhint errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,10 @@
+/*eslint-disable */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-    devServer: {
-    static: './dist',
-  },
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
