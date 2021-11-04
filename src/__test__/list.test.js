bb7c0bb87c7d0c0bee41d3f735aa7a3aa8dcd5be
blessedjasonmwanza <mwanzabj@gmail.com>
2021-11-04 14:10:25 +0200

    Setup babel, Setup Jest, Add addTask test unit case

---
+++ src/__test__/list.test.js
@@ -0,0 +1,32 @@
+/**
+ * @jest-environment jsdom
+ */
+import List from '../list.js';
+
+document.body.innerHTML = `
+  <section class="d-flex cols todo-list">
+    <header class="header d-flex s-between">
+        <h2>Today's To Do </h2>
+        <span class="material-icons" id="delete-all">autorenew</span>
+    </header>
+    <form action="#" class="d-flex s-between" id="add-task">
+        <input type="text" name="activity" placeholder="Add your List">
+        <button type="submit">
+            <span class="material-icons">keyboard_return</span>
+        </button>
+    </form>
+    <ul id="list-items"></ul>
+    <div class="d-flex clear-completed">
+      Clear All Completed
+    </div>
+  </section>
+`;
+describe('add and remove', () => {
+  // mock Localstorage simulation
+  window.localStorage = Storage.prototype;
+  test('Add task', () => {
+    const todoList = new List();
+    todoList.addActivity('Test');
+    expect(todoList.list).toHaveLength(1);
+  });
+});
